04ceee47a0cb985ce02d4e6be7dc7486b82a9279
Losvuex <dmckg1999@gmail.com>
2023-05-03 16:49:28 +0300

    add simple server using python socket

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 import socket
 
-server_sock = socket.socket(socket.AF_INET,     
-                            socket.SOCK_STREAM,  
-                            proto=0)             
-print(type(server_sock))                         
+s = socket.socket()
+s.bind(('localhost', 8080)) # Привязываем серверный сокет к localhost и 8080 порту.
+s.listen(1) # Начинаем прослушивать входящие соединения
+conn, addr = s.accept() # Метод который принимает входящее соединение.
+
+while True: # Создаем вечный цикл.
+	data = conn.recv(1024) # Получаем данные из сокета.
+	if not data:
+		break
+	conn.sendall(data) # Отправляем данные в сокет.
+	print(data.decode('utf-8')) # Выводим информацию на печать.
+conn.close()
